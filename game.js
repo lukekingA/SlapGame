6b60db0039a8496c01205c28127b9f1f304a9e96
Luke King <lkingfrm@gmail.com>
2019-02-08 14:55:49 -0700

    html structure

---
+++ game.js
@@ -0,0 +1,24 @@
+const game = {
+  player: {
+    name: '',
+    health: 100,
+    attacks: {
+      punch: 5,
+      kick: 10,
+      slap: 1,
+      addons: 0
+    }
+  },
+  monster: {
+    name: '',
+    health: 100,
+    attacks: {
+      bite: 20,
+      claw: 10,
+      addons: 0
+    }
+  },
+  addons: {
+
+  }
+}
