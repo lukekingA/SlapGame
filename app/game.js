5c223bc58b84ced123a1edc4ab13534a02106f5b
Luke King <lkingfrm@gmail.com>
2019-02-09 23:52:59 -0700

    functionality complete

--- app/game.js
+++ app/game.js
@@ -1,129 +1,73 @@
-// const player = {
-//   name: "Soldier",
-//   strength: {
-//     health: 100,
-//     energy: 100
-//   },
-//   experience: 0,
-//   attacks: {
-//     punch: 5,
-//     kick: 10,
-//     slap: 1
-//   },
-//   addons: {
-//     slap: {
-//       set: false,
-//       value: 2
-//     },
-//     punch: {
-//       set: false,
-//       value: 5
-//     },
-//     armor: {
-//       set: false,
-//       value: -5
-//     }
-//   }
-// }
-
-const game = {
-  session: {
-    plays: 0,
-    random: 0,
-    monsterName: ""
+const player = {
+  name: "Soldier",
+  wins: 0,
+  strength: {
+    health: 100,
+    energy: 100
   },
-  games: {
-    count: 0
-  },
-  player: {
-    name: "Soldier",
-    strength: {
-      health: 100,
-      energy: 100
-    },
-    experience: 0,
-    attacks: {
-      punch: 5,
-      kick: 10,
-      slap: 1
-    }
-  },
-  monster: {
-    names: ["Ragnor", "Terrorificus", "Bill"],
-    strength: {
-      health: 100,
-      energy: 100, //affected by age, random
-      age: ""
-    },
-    hunger: 0,
-    attacks: {
-      bite: 10,
-      claw: 7
-    }
+  experience: 0,
+  attacks: {
+    punch: 5,
+    kick: 10,
+    slap: 1
   },
   addons: {
-    player: {
-      slap: {
-        set: false,
-        value: 2
-      },
-      punch: {
-        set: false,
-        value: 5
-      },
-      armor: {
-        set: false,
-        value: -5
+    slap: {
+      set: false,
+      value: 2
+    },
+    punch: {
+      set: false,
+      value: 5
+    },
+    armor: {
+      set: false,
+      value: -5
+    }
+  },
+  setAddons(event) {
+    let value = event.target.dataset.upgrade;
+    if (player.experience >= 4) {
+      player.addons[value].set = true;
+      event.target.disabled = true;
+      if (event.target.nextElementSibling) {
+        event.target.nextElementSibling.disabled = false;
       }
-    }
-  },
-  attack(event) {
-    let attack = event.target.id;
-    let value = game.player.attacks[attack];
-    if (game.addons.player[attack] && game.addons.player[attack].set == true) {
-      value += game.addons.player[attack].value;
-    }
-    game.session.plays++;
-    game.player.experience++;
-    if (game.monster.strength.health - value < 0) {
-      game.monster.strength.health = 0;
+      player.experience -= 4;
     } else {
-      game.monster.strength.health -= Math.floor(
-        value + value * ((100 - game.monster.strength.energy) / 100)
+      $(".col-6").prepend(
+        `<h5 id='drop'>You Need ${4 - player.experience} More Experience</h5>`
       );
+      setTimeout(() => {
+        $("#drop").remove();
+      }, 800);
     }
-    game.monsterAttack();
     game.updatePage();
-    if (!game.monster.strength.health || !game.player.strength.health) {
-      game.winGame();
-    }
+  }
+};
+
+const monster = {
+  names: ["Ragnor", "Terrorificus", "Bill"],
+  name: "",
+  strength: {
+    health: 100,
+    energy: 100, //affected by age, random
+    age: ""
   },
-  monsterAttack() {
-    let attackValue = game.monster.attacks.claw;
-    if (Math.ceil(Math.random() * 10) > 5) {
-      attackValue = game.monster.attacks.bite;
-    }
-    attackValue -= game.addons.player.armor.set
-      ? game.addons.player.armor.value
-      : 0;
-    attackValue += attackValue * (game.monster.hunger / 100);
-    if (game.player.strength.health - attackValue < 0) {
-      game.player.strength.health = 0;
-    } else {
-      game.player.strength.health -= Math.floor(attackValue);
-    }
-    console.log(attackValue);
+  hunger: 0,
+  attacks: {
+    bite: 18,
+    claw: 10
   },
   monsterAge() {
     let age = game.session.random * 5;
-    game.monster.strength.energy -= age + 15;
-    game.monster.strength.age =
-      age < 15 ? "strong" : age < 30 ? "young" : "old";
+    monster.strength.energy -= age + 15;
+    monster.strength.age = age < 15 ? "strong" : age < 30 ? "young" : "old";
   },
   monsterHunger() {
-    game.monster.hunger = game.session.random * 10;
+    monster.hunger = game.session.random * 10;
   },
-  makeMonsterName() {
+  monsterName() {
     let index = 0;
     if (game.session.random < 4) {
       index = 0;
@@ -132,43 +76,136 @@ const game = {
     } else {
       index = 2;
     }
-    game.session.monsterName = game.monster.names[index];
+    monster.name = monster.names[index];
   },
-  setAddons(event) {
-    let value = event.target.dataset.upgrade;
-    if (game.player.experience >= 7) {
-      game.addons.player[value].set = true;
-      event.target.disabled = true;
-      event.target.nextElementSibling.disabled = false;
-      game.player.experience -= 7;
-    } else {
-      $(".col-6").prepend(`<h5 id="drop">You Need 7 Experience</h5>`);
-      setTimeout(() => {
-        $("#drop").remove();
-      }, 800);
+  monsterAttack() {
+    let attackValue = monster.attacks.claw;
+    if (Math.ceil(Math.random() * 10) > 5) {
+      attackValue = monster.attacks.bite;
     }
+    attackValue -= player.addons.armor.set ? game.addons.player.armor.value : 0;
+    attackValue += attackValue * (monster.hunger / 100);
+    attackValue -= attackValue * (monster.strength.energy / 100);
+    if (player.strength.health - attackValue < 0) {
+      player.strength.health = 0;
+    } else {
+      player.strength.health -= Math.floor(attackValue);
+    }
+  }
+};
+
+const game = {
+  session: {
+    plays: 0,
+    random: 0
+  },
+  count: 0,
+  attack(event) {
+    let attack = event.target.id;
+    let value = player.attacks[attack];
+    if (player.addons[attack] && player.addons[attack].set == true) {
+      value += player.addons[attack].value;
+    }
+    value = Math.floor(value + value * ((100 - monster.strength.energy) / 100));
+    game.session.plays++;
+    player.experience++;
+    if (monster.strength.health - value < 0) {
+      monster.strength.health = 0;
+    } else {
+      monster.strength.health -= value;
+    }
+    monster.monsterAttack();
     game.updatePage();
+    if (!monster.strength.health || !player.strength.health) {
+      game.winGame();
+    }
   },
   winGame() {
-    alert("game over");
-    game.games.count++;
+    Array.from(document.querySelectorAll("#actions button")).forEach(button => {
+      button.disabled = true;
+    });
+    game.count++;
+    if (monster.strength.health < player.strength.health) {
+      player.wins++;
+    }
+    let winner = player.strength.health ? player.name : monster.name;
+    game.updatePage();
+    $("body").append(
+      `<div id="popup" class="card">
+        <div class="card-body">
+          <h4 class="card-title">${winner} Has Won</h4>
+          <button
+            id="replay"
+            class="btn btn-lg bg-dark text-white"
+            type="button"
+          onclick="game.replay()">
+            Play Again
+          </button>
+        </div>
+      </div>`
+    );
+  },
+  replay() {
+    player.strength.health = 100;
+    monster.strength.health = 100;
+    monster.strength.energy = 100;
+    monster.attacks.bite++;
+    monster.attacks.claw++;
+    monster.monsterName();
+    monster.monsterAge();
+    monster.monsterHunger();
+    Array.from(document.querySelectorAll("#actions button")).forEach(button => {
+      button.disabled = false;
+    });
+    game.updatePage();
+    if ($("#popup")) {
+      $("#popup").remove();
+    }
+  },
+  reset() {
+    game.replay();
+    player.experience = 0;
+    for (let addon in player.addons) {
+      player.addons[addon].set = false;
+    }
+    $("#sword").disabled = true;
+    $("#arrmor").disabled = true;
+    game.updatePage();
   },
   updatePage() {
-    $("#monsterName").text(game.session.monsterName);
-    $("#monsterAge").text(game.monster.strength.age);
-    $("#monsterHunger").text(game.monster.hunger);
-    $("#monsterHealth").text(game.monster.strength.health);
+    $("#monsterName").text(monster.name);
+    $("#monsterAge").text(monster.strength.age);
+    $("#monsterHunger").text(monster.hunger);
+    $("#monsterHealth").text(monster.strength.health);
+    if (monster.strength.health < 35) {
+      $("#monsterName~div>.progressBar span").css("background-color", "red");
+    } else {
+      $("#monsterName~div>.progressBar span").css(
+        "background-color",
+        "rgb(43, 194, 83)"
+      );
+    }
     $("#monsterName~div>.progressBar span").css(
       "width",
-      `${game.monster.strength.health}%`
+      `${monster.strength.health}%`
     );
+    if (player.strength.health < 35) {
+      $("#playerName~div>.progressBar span").css("background-color", "red");
+    } else {
+      $("#playerName~div>.progressBar span").css(
+        "background-color",
+        "rgb(43, 194, 83)"
+      );
+    }
     $("#playerName~div>.progressBar span").css(
       "width",
-      `${game.player.strength.health}%`
+      `${player.strength.health}%`
     );
-    $("#playerName").text(game.player.name);
-    $("#playerHealth").text(game.player.strength.health);
-    $("#playerExperience").text(game.player.experience);
+    $("#playerName").text(player.name);
+    $("#playerHealth").text(player.strength.health);
+    $("#playerExperience").text(player.experience);
+    $("#games").text(game.count);
+    $("#wins").text(player.wins);
   }
 };
 
@@ -179,14 +216,15 @@ function initialize() {
   });
   Array.from(document.querySelectorAll("#playerName~button")).forEach(
     button => {
-      button.addEventListener("click", game.setAddons);
+      button.addEventListener("click", player.setAddons);
     }
   );
+  document.getElementById("reset").addEventListener("click", game.reset);
   let random = Math.floor(Math.random() * 10);
   game.session.random = random;
-  game.monsterAge();
-  game.makeMonsterName();
-  game.monsterHunger();
+  monster.monsterAge();
+  monster.monsterName();
+  monster.monsterHunger();
   game.updatePage();
 
   let template = "";
